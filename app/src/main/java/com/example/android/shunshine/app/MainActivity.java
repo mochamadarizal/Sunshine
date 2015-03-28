4d1c4f1a99e4d84534e5129d0b4cc88dda314c65
Arizal <Rizalduasembilan@gmail.com>
2015-03-28 00:25:25 -0700

    Initial Commit

--- app/src/main/java/com/example/android/shunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/shunshine/app/MainActivity.java
@@ -10,6 +10,12 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -56,11 +62,39 @@ public class MainActivity extends ActionBarActivity {
         public PlaceholderFragment() {
         }
 
+        ArrayAdapter<String> mAdapterForcast;
+
+
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+               String[] data = {
+                        "MOn 6/23 - sunny - 31/17",
+                        "Tue 6/24 - sunny - 21/17",
+                        "Wed 6/25 - cloudy - 57/17",
+                        "Thurs 6/26 - sunny - 31/17",
+                        "Fry 6/27 - cloudy - 38/17",
+                        "Sat 6/28 - sunny - 78/17",
+                        "Sun 6/29 - Trapped - 37/17"
+            };
+
+            List<String> WeekForecast = new ArrayList<String>(Arrays.asList(data));
+
+            mAdapterForcast = new ArrayAdapter<String>(
+                    getActivity(), //id activity
+                    R.layout.list_item_forecast, //id layout
+                    R.id.list_item_forecast_textview, // ID text view
+                    WeekForecast //list data
+
+            );
+
+            ListView listView =(ListView) rootView.findViewById(R.id.listview_forecast);
+            listView.setAdapter(mAdapterForcast);
             return rootView;
+
+
         }
     }
 }
